c39fdf72cfdd8b91133a40d4ecdd5d79e0b5a92a
Eyrim <marfxedbot@gmail.com>
2023-11-23 21:03:02 +0000

    Add click handler for abilities

--- src/components/AgentProfileView.tsx
+++ src/components/AgentProfileView.tsx
@@ -9,21 +9,35 @@ interface Props {
 
 export default function App(props: Props): React.JSX.Element {
     const [abilities] = useState(props.profile.abilities);
+    const [selectedAbilities, setSelectedAbilities] = useState<Ability[]>([]);
 
     function renderAbilities(abilities: Ability[]): React.JSX.Element {
         return (
             <div className="agent-ability-container-container">
-                {[...abilities].map((ability) => {
-                    return (
-                        <div className="agent-ability-container">
-                            <div className="ability-item">{ability.name}</div>
-                            <div className="ability-item">{ability.cost}</div>
-                            <br/>
-                        </div>
-                    );
-                })}
+                {
+                    <table>
+                        <thead>
+                            <tr>
+                                <th className="ability-table-header">Ability Name</th>
+                                <th className="ability-table-header">Ability Cost</th>
+                            </tr>
+                        </thead>
+                        {abilities.map((ability) => {
+                            return (
+                                <tr onClick={abilityOnClick}>
+                                    <td className="ability-table-data">{ability.name}</td>
+                                    <td className="ability-table-data">{ability.cost}</td>
+                                </tr>
+                            );
+                        })}
+                    </table>
+                }
             </div>
         );
+    }
+
+    function abilityOnClick(event): void {
+        let element: HTMLTableRowElement = event.currentTarget;
     }
 
     return (
